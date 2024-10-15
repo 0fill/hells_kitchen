2a204d4d3dee3d6bdcaa410a5e8d5657f47d441b
0fill <fikrosek@seznam.cz>
2024-10-15 15:08:35 +0200

    basic pages

--- djangoProject1/core/views.py
+++ djangoProject1/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def HomePage(request):
+    return render(request, 'home.html')
+
+def AboutPage(request):
+    return render(request, 'about.html')
+
+def ContactPage(request):
+    return render(request, 'contact.html')
