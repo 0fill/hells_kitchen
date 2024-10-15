0f7f0c3460d7fd5df9dc6781f98a9ea6ec6e2dd5
0fill <fikrosek@seznam.cz>
2024-10-15 16:02:03 +0200

    models
    made models + migration, functions for it will come next

--- djangoProject1/core/views.py
+++ djangoProject1/core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import User
 
 
 # Create your views here.
@@ -11,3 +13,18 @@ def AboutPage(request):
 
 def ContactPage(request):
     return render(request, 'contact.html')
+
+def add_fav(request):
+    if request.method == 'POST':
+        data = request.POST
+        name = data.get('name')
+        food_id = data.get('id')
+        User.favorite_food.update({food_id:name})
+        return redirect('/')
+
+
+def delete_url(request, id):
+    queryset = User.objects.get(id=id)
+    queryset.delete()
+    return redirect('/')
+
--- djangoProject1/recipes/models.py
+++ djangoProject1/recipes/models.py
@@ -1,3 +1,11 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    difficulty = models.PositiveSmallIntegerField(default=1,
+                                                  validators=[MinValueValidator(1), MaxValueValidator(10)])
